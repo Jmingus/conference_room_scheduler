664604a659283a3dc9122a4637a999823793a597
Jmingus <jmingus@austin.rr.com>
2015-02-23 23:21:36 -0600

    Added Readme and fixed seeds

---
+++ README.md
@@ -0,0 +1,24 @@
+
+
+<h1>Conference Scheduler App</h1>
+----------
+
+## Things that are done. ##
+		
+     - Users can sign-in and sign-out.
+     - Create Meetings in a Room.
+     - Pagination.
+     - AWS and paperclip working.
+
+## Things that need to be done ##
+	
+	
+
+     - Mailer working
+     - Display user that created meeting
+     - Make meeting form look better
+     - Make meetings in time durations
+     - Add validations
+	 - Make things redirect correctly
+	
+[Heroku-Link](https://conference-83242.herokuapp.com/)
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,40 +8,40 @@
  Room.create([{ room_name: 'The Library of Rails Knowledge',
                  room_location: 'Building C',
                  room_description: 'Students pour over their technology books, to learn one more feature they can cram in to the app.',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112234.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/001/medium/20150220_112234.jpg?1424728224'
                },
                { room_name: 'The Design Room',
                  room_location: 'Building C',
                  room_description: 'Smoke pours out from under their door, for they are SASS wizards!',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112247.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/002/medium/20150220_112247.jpg?1424729847'
                },
                { room_name: 'The Kitchen of Foodstuffs',
                  room_location: 'Building C',
                  room_description: 'Weird place to have a conference, all things considered.',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112253.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/003/medium/20150220_112253.jpg?1424729923'
                },
                { room_name: 'Blurry Photo Room',
                  room_location: 'Building C',
                  room_description: 'Great place for conferences, maybe, hard to see much',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112257.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/004/medium/20150220_112257.jpg?1424729997'
                },
                { room_name: 'The Forgotten Room',
                  room_location: 'Building K',
                  room_description: 'Bats fly in and out, bringing cigar smoke with them, conference at your own risk.',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112547.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/005/medium/20150220_112547.jpg?1424730975'
                },
                { room_name: 'The Yellow Room',
                  room_location: 'Building K',
                  room_description: 'The perfect room for sitting down, and getting a conference out of the way.',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112556.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/006/medium/20150220_112556.jpg?1424731018'
                },
                { room_name: 'The Javascript Room',
                  room_location: 'Building K',
                  room_description: 'Not sure what goes on in this room, sure is big however.',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112613.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/007/medium/20150220_112613.jpg?1424731057'
                },
                { room_name: 'The Vault',
                  room_location: 'Building K',
                  room_description: 'Not sure how much food is stored here.',
-                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/Dev+Photos/20150220_112628.jpg'
+                 room_image: 'http://conferenceroom8662.s3.amazonaws.com/rooms/room_images/000/000/008/medium/20150220_112628.jpg?1424731093'
                }])
